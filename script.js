dbd42f6669decac3771e858b4ad508d80b24b31e
Vladislav <vladfyklev@gmail.com>
2022-05-13 12:16:55 +0300

    add modal form

---
+++ script.js
@@ -0,0 +1,25 @@
+let modal = document.querySelector(".modal")
+let btnCloseForm = document.querySelector(".btn-close")
+
+function closeModal() {
+  modal.classList.add("hide")
+  modal.classList.remove("show")
+}
+btnCloseForm.addEventListener("click", closeModal)
+
+function openModal() {
+  modal.classList.add("show")
+  modal.classList.remove("hide")
+}
+modal.addEventListener("click", function (e) {
+  if(e.target === modal) {
+      closeModal()
+  }
+})
+function showModalScroll() {
+  if(window.pageYOffset > document.body.scrollHeight / 2) {
+    openModal()
+    window.removeEventListener("scroll", showModalScroll)
+}
+}
+window.addEventListener("scroll",showModalScroll)
